3d7195e59a5603efab879eed66d0aafa76a963c3
gio <giogioshall@yahoo.com.tw>
2015-09-04 00:53:21 +0800

    ok

--- js/script.js
+++ js/script.js
@@ -2,21 +2,21 @@ var wHeight = $(window).height();
 $(window).scroll(function(){
 	var scroll = $(window).scrollTop();
     //console.log(scroll)
-    if ( scroll > wHeight/2 ) {	
+    if ( scroll >= wHeight*0.9 ) {	
     	$('.phone').addClass('sc-02');
     	$('.game').addClass('show');  	
     } else {
     	$('.phone').removeClass('sc-02');
     	$('.game').removeClass('show');
     }
-    if ( scroll > wHeight*1.8 ) {
+    if ( scroll >= wHeight*1.9 ) {
     	$('.phone').addClass('sc-03');
     	$('.show').addClass('store'); 	
     } else {
     	$('.phone').removeClass('sc-03');
     	$('.show').removeClass('store');
     }
-    if ( scroll > wHeight*3.8 ) {
+    if ( scroll >= wHeight*3.9 ) {
         $('.phones').show();
         $('.phone').delay(1000).hide();  
     } else {
@@ -27,11 +27,11 @@ $(window).scroll(function(){
     if ( $('.phones').attr('style') == 'display: block;' ) { 
         $('.phones ul').addClass('list');
         $('.growth span').addClass('show-g');
-        $('.phones,.growth,.section-05 .title').addClass('slide');
+        $('.phones,.growth,.section-05 .title,.more').addClass('slide');
     } else {
         $('.phones ul').removeClass('list');
         $('.growth span').removeClass('show-g');
-        $('.phones,.growth,.section-05 .title').removeClass('slide');
+        $('.phones,.growth,.section-05 .title,.more').removeClass('slide');
     }
 });
 
@@ -40,14 +40,49 @@ for (var i = 1; i <= 15; i++) {
 };
 
 
-jQuery(document).ready(function($) {
+$(document).ready(function($) {
+$(window).scrollTop(0);
+$('.phones,.growth').removeClass('fade');
 $body = (window.opera) ? (document.compatMode == "CSS1Compat" ? $('html') : $('body')) : $('html,body');
 });
-function cpclick1(){$body.animate( { scrollTop: $('.section-01').offset().top}, 500)};
-function cpclick2(){$body.animate( { scrollTop: $('.section-02').offset().top}, 500)};
-function cpclick3(){$body.animate( { scrollTop: $('.section-03').offset().top}, 500)};
-function cpclick5(){$body.animate( { scrollTop: $('.section-05').offset().top}, 500)};
-
+function cpclick1(){
+    $body.animate( { scrollTop: $('.section-01').offset().top}, 500);
+    $('.phones,.growth,.sub-nav').removeClass('fade');
+    $('.nav li').removeClass('active');
+    $('.sub-nav li:nth-of-type(1)').addClass('active');
+};
+$('.phone').after().click(function(){
+    $body.animate( { scrollTop: $('.section-02').offset().top}, 500);
+    $('.nav li').removeClass('active');
+    $('.sub-nav li:nth-of-type(2)').addClass('active');
+});
+function cpclick2(){
+    $body.animate( { scrollTop: $('.section-02').offset().top}, 500);
+    $('.nav li').removeClass('active');
+    $('.sub-nav li:nth-of-type(2)').addClass('active');
+};
+function cpclick3(){
+    $body.animate( { scrollTop: $('.section-03').offset().top}, 500);
+    $('.nav li').removeClass('active');
+    $('.sub-nav li:nth-of-type(3)').addClass('active');
+};
+function cpclick5(){
+    $body.animate( { scrollTop: $('.section-05').offset().top}, 500);
+    $('.nav li').removeClass('active');
+    $('.sub-nav li:nth-of-type(4)').addClass('active');
+};
+function cpclick6(){
+    $body.animate( { scrollTop: $('.section-06').offset().top}, 500);
+    $('.phones,.growth,.sub-nav').addClass('fade');
+    $('.nav li').removeClass('active');
+    $('.nav li:nth-of-type(2)').addClass('active');
+};
+function cpclick7(){
+    $body.animate( { scrollTop: $('.section-07').offset().top}, 500);
+    $('.phones,.growth,.sub-nav').addClass('fade');
+    $('.nav li').removeClass('active');
+    $('.nav li:nth-of-type(3)').addClass('active');
+};
 
 $({countNum: $('.counter').text()}).animate({countNum: 999999}, {
   duration: 6000,
